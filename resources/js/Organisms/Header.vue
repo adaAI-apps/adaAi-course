a514ca324e1cae80408932ddae35f5b019c1478b
adaAI apps <angga.adaai@gmail.com>
2025-02-07 23:25:28 +0700

    initial commit

---
+++ resources/js/Organisms/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="fixed w-full py-4">
+        <div class="container flex items-center justify-between">
+            <!-- left header -->
+            <div class="flex items-center gap-x-3">
+                <div class="w-8">
+                    <img src="/images/logo.png" alt="logo" />
+                </div>
+                <h4 class="text-xl font-bold">adaAI</h4>
+            </div>
+
+            <!-- right header -->
+             <div>
+                <button>
+                </button>
+                 <button>
+                     <RiMenuLine/>
+                 </button>
+             </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import {RiMenuLine} from '@remixicon/vue'
+</script>
